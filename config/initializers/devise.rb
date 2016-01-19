6025eeda856a7142a27ab208c006f97768b78b93
Diogoazvg <diogo.azvg@gmail.com>
2016-01-19 09:31:25 -0200

    initial commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,11 +265,12 @@ Devise.setup do |config|
   require "omniauth-facebook"
   
   #No ar
-  config.omniauth :facebook, "1676686979209593", "aca40fc8294bdce4d9015b3d94a49f28", 
-    scope: 'email', info_fields: 'email,name,first_name,last_name,gender'
+  #config.omniauth :facebook, "1676686979209593", "aca40fc8294bdce4d9015b3d94a49f28", 
+    #scope: 'email', info_fields: 'email,name,first_name,last_name,gender'
 
   #Local
   #config.omniauth :facebook, "761409747325218", "8cdb9a43abfb8b9c9e2d737d43d46423"
 
+config.omniauth :facebook, ENV['1676686979209593'], ENV['aca40fc8294bdce4d9015b3d94a49f28'], scope: 'email', info_fields: 'email, name'
   #ENV['RAILS_ENV'] ||= 'production'
 end
