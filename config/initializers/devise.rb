2ec8e6b1ed1c0ebe686888b47ab1a83fd209a05c
Diogoazvg <diogo.azvg@gmail.com>
2016-01-19 09:16:13 -0200

    initial commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   #config.omniauth :facebook, "761409747325218", "8cdb9a43abfb8b9c9e2d737d43d46423"
 
 
-  config.omniauth :facebook, ENV['1676686979209593'], ENV['aca40fc8294bdce4d9015b3d94a49f28'], 
+  config.omniauth :facebook, ENV[1676686979209593], ENV[aca40fc8294bdce4d9015b3d94a49f28], 
     scope: 'email', info_fields: 'email,name,first_name,last_name,gender'
 
   #ENV['RAILS_ENV'] ||= 'production'
