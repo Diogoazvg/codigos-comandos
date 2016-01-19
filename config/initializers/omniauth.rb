44d08ab666e5ecee2804cd8a3deb465a056b015d
Diogoazvg <diogo.azvg@gmail.com>
2016-01-19 12:53:40 -0200

    initial commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
   	:scope => 'email',
   		:client_options => {
-      		:site => 'https://codigos-comandos.herokuapp.com',
-      			:authorize_url => "https://facebook.com/dialog/oauth/"
+      		:site => 'https://facebook.com/dialog/oauth/',
+      			:authorize_url => "https://codigos-comandos.herokuapp.com"
     }
 end
+
+
