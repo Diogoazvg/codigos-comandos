0a97c6933c7f4dbc4e20fb56305b6446918a0605
Diogoazvg <diogo.azvg@gmail.com>
2016-01-19 12:36:11 -0200

    initial commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -278,4 +278,12 @@ Devise.setup do |config|
   #scope: 'email', info_fields: 'email, name'
   
   #ENV['RAILS_ENV'] ||= 'production'
+
+  use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
+    :client_options => {
+      :site => 'https://codigos-comandos.herokuapp.com',
+      :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
+    }
+end
 end
