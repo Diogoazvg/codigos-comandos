a2a0a5439c366b5f119417e6f11252e3316d8ef1
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 19:43:53 -0200

    g+plus

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -23,8 +23,8 @@ require "omniauth-google-oauth2"
 Rails.application.config.middleware.use OmniAuth::Builder do
   #Heroku
   provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
-  provider :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
-    { access_type: "offline", approval_prompt: "" }
+  #provider :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
+   # { access_type: "offline", approval_prompt: "" }
 end
 
 
