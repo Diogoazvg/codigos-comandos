7fe1ccc5f51c46537f790f7f151ef73d59cdc95d
Diogoazvg <diogo.azvg@gmail.com>
2016-01-19 22:16:49 -0200

    initial commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -13,7 +13,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
     provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  else
-    provider :facebook, 'DEPLOY_APP_ID', 'DEPLOY_APP_SECRET'
+  #else
+    #provider :facebook, 'DEPLOY_APP_ID', 'DEPLOY_APP_SECRET'
   end
 end
