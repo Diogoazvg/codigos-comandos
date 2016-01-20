53a0151c8348139bd54ad29a52622a117779b8ce
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 19:55:47 -0200

    g+plus

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,10 +271,10 @@ Devise.setup do |config|
 
   #ENV['RAILS_ENV'] ||= 'production'
   #Heroku
-  #require "omniauth-facebook"
+  require "omniauth-facebook"
   #config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 
-  #require "omniauth-google-oauth2"
+  require "omniauth-google-oauth2"
   #config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
     #{ access_type: "offline", approval_prompt: "" }
  
