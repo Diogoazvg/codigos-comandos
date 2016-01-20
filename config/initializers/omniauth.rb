7c554c8261a6dd3d55ea3eeaf07b2edd9038fac8
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 20:41:30 -0200

    g+plus

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 require "omniauth-facebook"
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
-
 require "omniauth-google-oauth2"
+config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
   { access_type: "offline", approval_prompt: "" }
