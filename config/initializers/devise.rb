7986953f3c210ce64d2e5dadf40e26a4a163b63d
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 19:39:25 -0200

    g+plus

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,11 +272,11 @@ Devise.setup do |config|
   #ENV['RAILS_ENV'] ||= 'production'
   #Heroku
   require "omniauth-facebook"
-  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  #config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 
   require "omniauth-google-oauth2"
-  config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
-    { access_type: "offline", approval_prompt: "" }
+  #config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
+    #{ access_type: "offline", approval_prompt: "" }
 
 
   #Local
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,30 @@
+#Rails.application.config.middleware.use OmniAuth::Builder do
+ # provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
+  #	:scope => 'email',
+  #		:client_options => {
+   #   		:site => 'https://facebook.com/dialog/oauth/',
+    #  			:authorize_url => "https://codigos-comandos.herokuapp.com"
+#    }
+#end
+
+#OmniAuth.config.logger = Rails.logger
+
+#Rails.application.config.middleware.use OmniAuth::Builder do
+ # if Rails.env.development?
+  #  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  #  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  #else
+   # provider :facebook, 
+   # scope: 'email'
+  #end
+#end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  #Heroku
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  provider :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
+    { access_type: "offline", approval_prompt: "" }
+end
+
+
+
