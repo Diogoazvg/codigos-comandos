34c45860c1bbe86594e7d36d8aae3ad727f72345
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 00:25:13 -0200

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   resources :commands
   resources :categories
   root to: 'commands#index'
-  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  devise_for :users, :controllers => { :omniauth_callbacks => 'authentications', :registrations => 'registrations' }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => 'authentications', :registrations => 'registrations' }
   resources :users
-
-  #devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
-#controllers: {omniauth_callbacks: "authentications", registrations: "registrations"}
 end
