98d95a931ff86ced45137265aeb58c9fab2650f8
Diogoazvg <diogo.azvg@gmail.com>
2016-01-20 20:42:38 -0200

    g+plus

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,11 +271,11 @@ Devise.setup do |config|
 
   #ENV['RAILS_ENV'] ||= 'production'
   #Heroku
-  #require "omniauth-facebook"
-  #config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+  require "omniauth-facebook"
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
 
-  #require "omniauth-google-oauth2"
-  #config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
-    #{ access_type: "offline", approval_prompt: "" }
+  require "omniauth-google-oauth2"
+  config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
+    { access_type: "offline", approval_prompt: "" }
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-require "omniauth-facebook"
-require "omniauth-google-oauth2"
-config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
-config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
-  { access_type: "offline", approval_prompt: "" }
+#require "omniauth-facebook"
+#require "omniauth-google-oauth2"
+#config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+#config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
+ # { access_type: "offline", approval_prompt: "" }
