fc2e3a28a57ca223e685e2d55afa231a999e6877
Diogoazvg <diogo.azvg@gmail.com>
2016-01-21 06:28:02 -0200

    interface gráfica

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  RAILS_ENV=production rake assets:precompile
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
