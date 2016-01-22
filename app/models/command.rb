7441b62f5545256fc0ed1888c99944dd69747f47
Diogoazvg <diogo.azvg@gmail.com>
2016-01-22 01:25:05 -0200

    Validates

--- app/models/command.rb
+++ app/models/command.rb
@@ -2,6 +2,7 @@ class Command < ActiveRecord::Base
   belongs_to :category
   validates_presence_of :name, :category, :description, :command
   validates :command, uniqueness: true
+  validates :name, uniqueness: true
 
   scope :search, -> (busca){where("name like ?", "%#{busca}%")
   }
