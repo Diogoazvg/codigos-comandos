1fb389906fb285a1c6e74b37feb5d81de52b2038
Diogoazvg <diogo.azvg@gmail.com>
2016-01-22 09:12:07 -0200

    validates e login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true
+  validates_presence_of :name
+
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
